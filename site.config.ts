3165712dcfae5ba07a3b0bad024c2d9d831c844d
ProfileZang <101622930+ProfileZang@users.noreply.github.com>
2022-04-27 15:50:01 +0800

    replace url

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: 'f5c0a70420aa4b0c9bf4ec26a0af5da9',
+  rootNotionPageId: 'ba21b1e1739b4bbeae14a8736d84ab6b',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -10,7 +10,7 @@ export default siteConfig({
 
   // basic site info (required)
   name: 'Next.js Notion Starter Kit',
-  domain: 'https://cliff-earthworm-de5.notion.site/TXZ-Blog-f5c0a70420aa4b0c9bf4ec26a0af5da9',
+  domain: 'https://cliff-earthworm-de5.notion.site/TXZ-BLOG-ba21b1e1739b4bbeae14a8736d84ab6b',
   author: 'Travis Fischer',
 
   // open graph metadata (optional)
